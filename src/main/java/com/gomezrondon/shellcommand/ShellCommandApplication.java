e5a2fadbedbd20e2f8563b2134ef5035741a338f
DESKTOP-8NG6QPL\gomez <gomezrondon@gmail.com>
2018-04-28 16:36:10 -0600

    1rst Comand mock.

--- src/main/java/com/gomezrondon/shellcommand/ShellCommandApplication.java
+++ src/main/java/com/gomezrondon/shellcommand/ShellCommandApplication.java
@@ -2,6 +2,12 @@ package com.gomezrondon.shellcommand;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class ShellCommandApplication {
@@ -9,4 +15,36 @@ public class ShellCommandApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShellCommandApplication.class, args);
 	}
+}
+
+@ShellComponent
+class shellCommand{
+
+	private final CommandService commandService;
+
+	shellCommand(CommandService commandService) {
+		this.commandService = commandService;
+	}
+
+	@ShellMethod("Input numer Option")
+	public void command(int numberOption) {
+		this.commandService.selectCommandOption(numberOption);
+	}
+}
+
+@Service
+class CommandService{
+
+	private final Map<Integer, String> commands = new HashMap<>();
+
+	public CommandService(){
+		commands.put(1, "docker + Migration");
+		commands.put(2, "docker up");
+		commands.put(3, "docker down");
+		commands.put(4, "Migration");
+	}
+	void selectCommandOption(int numerOption){
+		System.out.println("Option selected: "+numerOption);
+	}
+
 }
